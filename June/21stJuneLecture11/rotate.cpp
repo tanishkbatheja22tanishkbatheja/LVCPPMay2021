1911c2e24759a9b795920ee73a13422ef43bcdc8
tanishkbatheja22tanishkbatheja <74583260+tanishkbatheja22tanishkbatheja@users.noreply.github.com>
2021-06-23 16:39:21 +0530

    Update rotate.cpp

--- June/21stJuneLecture11/rotate.cpp
+++ June/21stJuneLecture11/rotate.cpp
@@ -7,12 +7,28 @@ Example :
 	Input : n = 3, str[] = "codingblocks"
 	Output: ckscodingblo
 */
-#include<iostream>
 
+
+int main() {
+#include<bits/stdc++.h>
 using namespace std;
+
 
 int main() {
 
+int d;
+cin>>d;
+
+char s[20]="codingblocks";
+int n=strlen(s);
+reverse(s,s+(n-d));
+
+reverse(s+(n-d),s+n);
+
+reverse(s,s+n);
+
+cout<<s<<endl;
 
 	return 0;
 }
+
